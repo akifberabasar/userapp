84065b32fa28189929c343ef0ecc5397394b592f
akifbsr <akifberabasar@gmail.com>
2022-09-30 10:16:57 +0300

    reactdeneme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component} from 'react';
+import AddUser from './components/AddUser';
+import Users from './components/Users';
+
+
+class  App extends Component{
+  
+
+ deleteUser(id){
+  let updatedUsers = this.state.users;
+
+  updatedUsers = updatedUsers.filter(user => user.id !== id);
+
+  this.setState({
+    users: updatedUsers
+  })
+ }
+
+ constructor(props) {
+  super(props);
+
+  this.state = {
+    users: [
+      {
+        id : 1,
+        name : "Mustafa Murat Coşkun",
+        email: "coskun@gmail.com"
+      }, 
+      {
+        id : 2,
+        name : "Oğuz Artıran",
+        email: "oguz@gmail.com"
+      },
+      {
+        id : 3,
+        name : "Tolgahan Gencer",
+        email: "gencer@gmail.com"
+      }
+    ] 
+  };
+  this.deleteUser = this.deleteUser.bind(this);
+  this.AddUser = this.AddUser.bind(this);
+ }
+  AddUser(newUser) {
+    let updatedUsers = this.state.users;
+
+    updatedUsers.push(newUser);
+
+    this.setState({
+      users: updatedUsers
+    })
+  }
+
+  render() {
+   return (
+     <div className="container">
+        <h4>User App</h4>
+        <hr />
+        <AddUser AddUser={this.AddUser} />
+        <hr />
+        <Users deleteUser = {this.deleteUser}     users = {this.state.users} />
+       
+      </div>
+    );
+   }
+  
 }
 
 export default App;
